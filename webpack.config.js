2d3d9645152c68f4f82a8a6a0a0b5459048564bd
Andrew Charette <andrew@andrews-mbp.mynetworksettings.com>
2023-08-18 19:52:34 -0400

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        // entry point of our app
+        './client/index.js',
+      ],
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js',
+      }, 
+    mode: 'development',
+    module: {
+        rules: [
+          {
+            test: /.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+            options: {
+                presets:  ['@babel/preset-env', '@babel/preset-react']
+                //plugins: ['@babel/plugin-transform-runtime', '@babel/transform-async-to-generator'],
+              }
+          },
+          {
+            test: /.(css|scss)$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader'],
+          }
+        ],
+      },
+      devServer: {
+        host: 'localhost',
+        port: 8080,
+        // enable HMR on the devServer
+        hot: true,
+        // fallback to root for other urls
+        historyApiFallback: true,
+    
+        static: {
+          // match the output path
+          directory: path.resolve(__dirname, 'dist'),
+          // match the output 'publicPath'
+          publicPath: '/',
+        },
+    
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        /**
+         * proxy is required in order to make api calls to
+         * express server while using hot-reload webpack server
+         * routes api fetch requests from localhost:8080/api/* (webpack dev server)
+         * to localhost:3000/api/* (where our Express server is running)
+         */
+        proxy: {
+          '/notes/**': {
+            target: 'http://localhost:3000/',
+            secure: false,
+          },
+          '/login/**': {
+            target: 'http://localhost:3000/',
+            secure: false,
+          },
+          '/signup/**': {
+            target: 'http://localhost:3000/',
+            secure: false,
+          },
+        },
+      },
+}
